06144325e82ab7ca336fe636f2cdded28181a33e
Nerze <y.cottin@hotmail.fr>
2022-05-22 17:35:53 +0200

    Changement du contexte.message.content en message

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ async def on_message(contexte,message:str):
     if contexte.author.id == bot.user.id:
         pass
     else:
-        rechercheMsg = contexte.message.content
-        if "ta gueule" in rechercheMsg:
+        if "ta gueule" in message:
             await contexte.channel.send("le language svp")
-        print(contexte.guild, ":", contexte.channel, ":", contexte.author, ":", contexte.message.content)
+        print(contexte.guild, ":", contexte.channel, ":", contexte.author, ":", message)
         if message == "test":
             await contexte.channel.send("test fonctionnel")
 
